01fe8aa6809be5854197ed329b159307b24b27c3
Gary <Gary.Dix@enform.nz>
2022-09-13 10:15:18 +1200

    Add project files.

---
+++ Models/Visitors.cs
@@ -0,0 +1,17 @@
+﻿namespace VisitorManagementStudent2022.Models
+{
+    public class Visitors
+    {
+        public Guid Id { get; set; }
+        public string FirstName { get; set; }
+        public string lastName { get; set; }
+        public DateTime DateIn { get; set; }
+        public DateTime DateOut { get; set; }
+
+        //navigation
+
+        public Guid StaffnameId { get; set; }
+        public StaffNames StaffName { get; set; }
+
+    }
+}
